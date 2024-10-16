952d50b19fa8b364492195bd7e2d662e0e6f9e9c
Maydel-c <165515798+Maydel-c@users.noreply.github.com>
2024-10-16 12:27:01 +0530

    cleanup comments

--- SimpleMBComp/Source/PluginEditor.cpp
+++ SimpleMBComp/Source/PluginEditor.cpp
@@ -351,47 +351,6 @@ releaseSlider(nullptr, "ms", "RELEASE"),
 thresholdSlider(nullptr, "dB", "THRESH"),
 ratioSlider(nullptr, ""/*, "RATIO"*/) // we are getting the 3rd parameter in this from RatioSlider class
 {
-    
-//    using namespace Params;
-//    const auto& params = GetParams();
-//    BELOW CODE NOW RESIDES IN updateAttachments()
-//    auto getParamHelper = [&params, &apvts = this->apvts](const auto& name) -> auto&
-//    {
-//        return getParam(apvts, params, name);
-//    };
-//
-//    // unlike GC, here we need reference because the function returns a reference. so '&' is added
-//    attackSlider.changeParam(&getParamHelper(Names::Attack_Mid_Band));
-//    releaseSlider.changeParam(&getParamHelper(Names::Release_Mid_Band));
-//    thresholdSlider.changeParam(&getParamHelper(Names::Threshold_Mid_Band));
-//    ratioSlider.changeParam(&getParamHelper(Names::Ratio_Mid_Band));
-//
-//    addLabelPairs(attackSlider.labels, getParamHelper(Names::Attack_Mid_Band), "ms");
-//    addLabelPairs(releaseSlider.labels, getParamHelper(Names::Release_Mid_Band), "ms");
-//    addLabelPairs(thresholdSlider.labels, getParamHelper(Names::Threshold_Mid_Band), "dB");
-//
-//    ratioSlider.labels.clear();
-//    ratioSlider.labels.add({0.f, "1:1"});
-//    auto ratioParam = dynamic_cast<juce::AudioParameterChoice*>(&getParamHelper(Names::Ratio_Mid_Band));
-////    ratioSlider.labels.add({1.f, ratioParam->choices.getReference(ratioParam->choices.size() - 1)}); // getting the last element
-//
-//    // getting the last element and displaying it in required format "x:1"
-//    ratioSlider.labels.add({1.f,
-//        juce::String(ratioParam->choices.getReference(ratioParam->choices.size() - 1).getIntValue()) + ":1" });
-//
-//
-//    auto makeAttachmentHelper = [&params, &apvts = this->apvts](auto& attachment,
-//                                                  const auto& name,
-//                                                  auto& slider)
-//    {
-//        makeAttachment(attachment, apvts, params, name, slider);
-//    };
-//
-//    makeAttachmentHelper(attackSliderAttachment, Names::Attack_Mid_Band, attackSlider);
-//    makeAttachmentHelper(releaseSliderAttachment, Names::Release_Mid_Band, releaseSlider);
-//    makeAttachmentHelper(thresholdSliderAttachment, Names::Threshold_Mid_Band, thresholdSlider);
-//    makeAttachmentHelper(ratioSliderAttachment, Names::Ratio_Mid_Band, ratioSlider);
-    
     addAndMakeVisible(attackSlider);
     addAndMakeVisible(releaseSlider);
     addAndMakeVisible(thresholdSlider);
@@ -404,10 +363,6 @@ ratioSlider(nullptr, ""/*, "RATIO"*/) // we are getting the 3rd parameter in thi
     addAndMakeVisible(bypassButton);
     addAndMakeVisible(soloButton);
     addAndMakeVisible(muteButton);
-    
-//    makeAttachmentHelper(bypassButtonAttachment, Names::Bypass_Mid_Band, bypassButton);
-//    makeAttachmentHelper(soloButtonAttachment, Names::Solo_Mid_Band, soloButton);
-//    makeAttachmentHelper(muteButtonAttachment, Names::Mute_Mid_Band, muteButton);
     
     lowBand.setName("Low");
     midBand.setName("Mid");
@@ -467,9 +422,7 @@ void CompressorBandControls::resized()
     flexBox.flexDirection = FlexBox::Direction::row;
     flexBox.flexWrap = FlexBox::Wrap::noWrap;
     
-    auto spacer = FlexItem().withWidth(4); // between each item
-//    auto endCap = FlexItem().withWidth(6); // space at ends of array
-//    flexBox.items.add(endCap);
+    auto spacer = FlexItem().withWidth(4);
     flexBox.items.add(spacer);
     flexBox.items.add(FlexItem(bandSelectControlBox).withWidth(50));
     flexBox.items.add(spacer);
@@ -480,7 +433,6 @@ void CompressorBandControls::resized()
     flexBox.items.add(FlexItem(thresholdSlider).withFlex(1.f));
     flexBox.items.add(spacer);
     flexBox.items.add(FlexItem(ratioSlider).withFlex(1.f));
-//    flexBox.items.add(endCap);
     flexBox.items.add(spacer);
 
     flexBox.items.add(FlexItem(bandButtonControlBox).withWidth(30));
